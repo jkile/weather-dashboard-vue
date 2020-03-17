22562e81c2223294b9562e5693db5dd876ab56fd
Jake Kile <jkile9@gmail.com>
2020-03-17 03:01:35 -0700

    brought html over and began separating templates into components

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,71 @@
+<template>
+  <div>
+    <div class="columns is-vcentered">
+
+      <Search/>
+
+      <div class="display-wrapper">
+        <div class="column">
+          <div class="columns">
+            <div class="column">
+              <div class="box" id="weather-display"></div>
+            </div>
+          </div>
+          <div class="columns">
+            <div class="column">
+              <h1 class="is-size-2">5-Day Forecast:</h1>
+            </div>
+          </div>
+          <div class="columns" id="forecastRow"></div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Search from "./Search.vue";
+export default {
+  name: "Dashboard",
+  props: {
+    msg: String
+  },
+  components: {
+    Search
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+html {
+  height: 100%;
+}
+body {
+  min-height: 100%;
+}
+.column {
+  flex-direction: column;
+  padding: 12px;
+}
+p {
+  margin: 12px;
+}
+.menu-wrapper {
+  padding-left: 10px;
+}
+.display-wrapper {
+  padding-right: 10px;
+}
+
+.wrapper {
+  padding: 12px;
+}
+
+.rounded {
+  border-radius: 15%;
+}
+.stretch {
+  min-height: 100%;
+}
+</style>
---
+++ src/components/Search.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="column has-background-light is-flex is-one-third">
+    <div class="menu-wrapper" id="menu">
+      <h1 class="is-size-2">Search for a City:</h1>
+      <div class="field is-grouped">
+        <p class="control is-expanded">
+          <input class="input" type="text" id="searchBox" placeholder="Enter City" />
+        </p>
+        <p class="control">
+          <button class="button is-info" id="searchButton">Search</button>
+        </p>
+      </div>
+      <button class="button is-large is-fullwidth">Austin</button>
+      <button class="button is-large is-fullwidth">Chicago</button>
+      <button class="button is-large is-fullwidth">New York</button>
+      <button class="button is-large is-fullwidth">Orlando</button>
+      <button class="button is-large is-fullwidth">San Francisco</button>
+      <button class="button is-large is-fullwidth">Seattle</button>
+      <button class="button is-large is-fullwidth">Denver</button>
+      <button class="button is-large is-fullwidth">Atlanta</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "Search"
+};
+</script>
+
+<style lang="stylus" scoped></style>
